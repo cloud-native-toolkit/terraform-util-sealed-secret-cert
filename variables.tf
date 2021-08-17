6d7f31091bed2800ccbf37970ab438f951982830
Sean Sundberg <seansund@us.ibm.com>
2021-08-17 07:32:11 -0500

    Initial version to generate a cert (#1)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -1,6 +1,23 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "cert" {
+  type        = string
+  description = "The public key that will be used to encrypt sealed secrets. If not provided, a new one will be generated"
+  default     = ""
+}
 
+variable "private_key" {
+  type        = string
+  description = "The private key that will be used to decrypt sealed secrets. If not provided, a new one will be generated"
+  default     = ""
+}
+
+variable "cert_file" {
+  type        = string
+  description = "The file containing the public key that will be used to encrypt the sealed secrets. If not provided a new public key will be generated"
+  default     = ""
+}
+
+variable "private_key_file" {
+  type        = string
+  description = "The file containin the private key that will be used to encrypt the sealed secrets. If not provided a new private key will be generated"
+  default     = ""
+}
